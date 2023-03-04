4cf15bdea0c8fd256919456dcdac7283ec7d0986
knowak013 <kamila.nowak013@gmail.com>
2023-03-04 17:55:39 +0100

    Set up requests

---
+++ src/requests.js
@@ -0,0 +1,6 @@
+export const getShowsByKey = key => {
+    return fetch(`https://api.tvmaze.com/search/shows?q=${key}`).then(resp => resp.json())
+}
+export const getShowById = id => {
+    return fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`).then(resp => resp.json())
+}
