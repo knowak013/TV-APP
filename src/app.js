311a443a68d6b4af6939e7ef9427b3a9794521f9
knowak013 <kamila.nowak013@gmail.com>
2023-03-12 23:25:49 +0100

    Started to display shows on list

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import {mapListToDOMElements} from "./domInteractions.js"
+import {getShowsByKey} from "./requests.js"
+import {createDOMElement} from "./domInteractions.js"
 
 class TvApp{
     constructor(){
@@ -10,6 +12,8 @@ class TvApp{
 
     initializeApp = () => {
         this.connectDOMElements()
+        this.setupListenners()
+        this.fetchAndDispalShows()
     }
 
     connectDOMElements = () => { 
@@ -26,14 +30,43 @@ class TvApp{
     setupListenners = () =>{
         
         Object.keys(this.showNameButtons).forEach(showName=>{
-            this.showNameButtons[showName].addEventListener('click', this.setCurrentNameFilter);
+            this.showNameButtons[showName].addEventListener('click', this.setCurrentNameFilter)
         })
 
        }
     setCurrentNameFilter = () =>{
-        this.selectedName = Event.target.dataset.showName;
+        this.selectedName = Event.target.dataset.showName
+        this.fetchAndDispalShows()
     }
 
+    fetchAndDispalShows = () =>{
+        getShowsByKey(this.selectedName).then(shows=> this.renderCard(shows))
+    }
+
+    renderCard = (shows) =>{
+        for(const {show} of shows){
+            this.createShowCard(show)
+        }
+    }
+
+    createShowCard = show=>{
+
+        const divCard = createDOMElement('div','card')
+        const img = createDOMElement('img','card-img-top',null, show.image.medium)
+        const divCardBody = createDOMElement('div','card-body')
+        const h5 = createDOMElement('h5','card-title',show.name)
+        const p = createDOMElement('p','card-text',show.summary)
+        const btn = createDOMElement('button','btn btn-primary','Show details')
+
+        divCard.appendChild(divCardBody);
+        divCardBody.appendChild(img);
+        divCardBody.appendChild(h5);
+        divCardBody.appendChild(p);
+        divCardBody.appendChild(btn);
+
+        this.ViewElems.showsWrapper.appendChild(divCard);
+
+    }
 }
 
 document.addEventListener("DOMContentLoaded", new TvApp())
--- src/domInteractions.js
+++ src/domInteractions.js
@@ -1,6 +1,8 @@
 const _getDomElem = (attribute,value) =>{
     return document.querySelector(`[${attribute}="${value}"]`);
 }
+const arr = [5,5,5];
+
 
 export const mapListToDOMElements = (listOfValues, attribute) =>{
     const _viewElems = {}
@@ -10,4 +12,15 @@ export const mapListToDOMElements = (listOfValues, attribute) =>{
     }
 
     return _viewElems;
+}
+
+export const createDOMElement = (tagName,className,innerText,src) => {
+
+    const tag = document.createElement(tagName)
+    tag.classList= className;
+
+    if (innerText)tag.innerText = innerText
+    if (src) tag.src =src
+
+    return tag
 }
